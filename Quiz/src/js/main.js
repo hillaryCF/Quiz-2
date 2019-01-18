ed6780b559b2571184d7a10a93fe9b24f7815737
Hilary Cordero Flores <h.cordero@WEB-07.local>
2019-01-18 11:31:14 -0600

    js

--- Quiz/src/js/main.js
+++ Quiz/src/js/main.js
@@ -1,54 +1,43 @@
-// calling the arrows
-// let arrow_left = document.getElementById("slider_arrow_left");
-// let arrow_right = document.getElementById("slider_arrow_right");
-
-// let con = 1;
-// arrow_right.addEventListener("click",next_event);
-
-// function next_event(){
-//     // let image2 = "img/bg-event-2.jpg";
-//     // let image = document.getElementById("container_slider");
-//     // let image_event2 = document.createElement("img");
-//     // image_event2.setAttribute("src",image2);
-//     // image_event2.setAttribute("id","slider_image");
-//     // // image_event2.setAttribute("width", "390");
-//     // image.appendChild(image_event2);
-    // let image4 = document.getElementsByClassName("slider_image");
-    // image4.style.display="none";
-//     for(con=1; true; con++){
-//         // let imagen = document.getElementById("container_slider");
-//         // let image1 = document.createElement("img");
-//         // image1.setAttribute("class","slider_image_first");
-//         console.log("ggg");
-//     }
-        
-        
-    
-// }   
-// function siguiente(){
-//     let image = document.getElementById("slider_image_first").style.display ="none";
-//     let new23 = document.getElementById("slider_image2").style.display="inline-block";
-// }
-
 let arrow_left = document.getElementById("slider_arrow_left");
 let arrow_right = document.getElementById("slider_arrow_right");
 let container_slider = document.getElementById("container_slider");
-let imagenes = document.getElementsByClassName("slider_image").length;
+let imagenes = [];
+let contador = 0;
 
+// array de imagenes
+imagenes[0] = "img/bg-event-2.jpg";
+imagenes[1]= "img/bg-event-3.jpeg";
+imagenes[2]= "img/bg-event-4.jpeg";
+imagenes[3]= "img/bg-event-5.jpeg";
 
 arrow_right.addEventListener("click", siguiente);
-let imagenestodo = [
-
-]
-
 for(i=0; i<=12 ; i++){
-    function siguiente(){  
+    function siguiente(i){
         arrow_right.onclick= function (){
             let image = document.getElementById("slider_image_first").style.display ="none";
-            let imagen2 = document.getElementsByClassName("slider_image").visibility ="inline-block";
-            // let imagenes = document.getElementsByClassName("slider_image").length;
-            
+            let imagen2 = document.getElementById("slider_image").style.display = "block";
         };
     }
 }
 siguiente();
+
+// Crea el request.
+const request = new XMLHttpRequest();
+
+// Registra el manejador de eventos.
+request.addEventListener('load', function (event) {
+  // Obtiene la respuesta.
+  const response = event.target.response;
+  console.log(response);
+  // Itera sobre los resultados y los agrega a la lista.
+
+})
+// Define el tipo de respuesta.
+request.responseType = 'json';
+
+// Inicializa el request.
+request.open('GET', '/event-api.json');
+
+// Envía el request.
+request.send();
+
